d836f6266bcf13ba99f0cdbe023d6725c399d3f9
Marcin Mielniczuk <marmistrz.dev@zoho.eu>
2024-05-18 12:48:14 +0200

    refactor encrypt

--- src/encrypt.rs
+++ src/encrypt.rs
@@ -1,3 +1,4 @@
+use std::ops::Add;
 use std::sync::Arc;
 
 use rand::thread_rng;
@@ -12,6 +13,11 @@ struct RCEnc<F: PrimeField> {
     rc: ReinforcedConcrete<F>,
 }
 
+enum Mode {
+    Encrypt,
+    Decrypt,
+}
+
 impl<F: PrimeField> RCEnc<F> {
     fn new(params: &Arc<ReinforcedConcreteParams<F>>) -> Self {
         Self {
@@ -21,18 +27,29 @@ impl<F: PrimeField> RCEnc<F> {
 
     fn encrypt(&self, message: Vec<F>, key: F) -> (F, Vec<F>) {
         let nonce = random_scalar_rng::<F, _>(false, &mut thread_rng());
+        self.encdec(message, nonce, key, Mode::Encrypt)
+    }
+
+    fn decrypt(&self, message: Vec<F>, nonce: F, key: F) -> Vec<F> {
+        self.encdec(message, nonce, key, Mode::Decrypt).1
+    }
+
+    fn encdec(&self, message: Vec<F>, nonce: F, key: F, mode: Mode) -> (F, Vec<F>) {
         let a = self.rc.hash(&nonce, &key);
 
-        message.into_iter().enumerate().map(|(i, msg_i)| {
-            let i_ff = i.into();
-            let r_i = self.rc.hash(&a, i_ff);
-            msg_i + r_i
-        });
-
-        let mut res = vec![];
-        for (i, m) in r.zip(message) {
-            res.push(m + i);
-        }
+        let res = message
+            .into_iter()
+            .enumerate()
+            .map(|(i, mut msg_i)| {
+                let i_ff = F::from_str(&i.to_string()).expect("field conversion failed");
+                let r_i = self.rc.hash(&a, &i_ff);
+                match mode {
+                    Mode::Encrypt => msg_i.add_assign(&r_i),
+                    Mode::Decrypt => msg_i.sub_assign(&r_i),
+                };
+                msg_i
+            })
+            .collect();
 
         (nonce, res)
     }
@@ -55,8 +72,18 @@ impl<F: PrimeField> RCEnc<F> {
 //     res
 // }
 
-#[test]
-fn run() {
-    let message = vec![random_scalar_rng::<FpBLS12, _>(false, &mut thread_rng()); 10];
-    let key = random_scalar_rng::<FpBLS12, _>(false, &mut thread_rng());
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test() {
+        let message = vec![random_scalar_rng::<FpBLS12, _>(false, &mut thread_rng()); 10];
+        let key = random_scalar_rng::<FpBLS12, _>(false, &mut thread_rng());
+
+        let rcenc = RCEnc::new(&RC_BLS_PARAMS);
+
+        let (nonce, enc) = rcenc.encrypt(message.clone(), key);
+        let dec = rcenc.decrypt(enc, nonce, key);
+        assert_eq!(message, dec)
+    }
 }
